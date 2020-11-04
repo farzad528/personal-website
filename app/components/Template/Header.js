21925c360669e3d4a8314997e80546b862bca7bf
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2020-11-04 14:13:46 -0600

    ko fi

--- app/components/Template/Header.js
+++ app/components/Template/Header.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Hamburger from './Hamburger';
-import routes from '../../data/routes';
+import Hamburger from "./Hamburger";
+import routes from "../../data/routes";
 
 const Header = () => (
   <header id="header">
     <h1 className="index-link">
-      {routes.filter((l) => l.index).map((l) => (
-        <Link key={l.label} to={l.path}>{l.label}</Link>
-      ))}
+      {routes
+        .filter((l) => l.index)
+        .map((l) => (
+          <Link key={l.label} to={l.path}>
+            {l.label}
+          </Link>
+        ))}
     </h1>
     <nav className="links">
       <ul>
-        {routes.filter((l) => !l.index).map((l) => (
-          <li key={l.label}>
-            <Link to={l.path}>{l.label}</Link>
-          </li>
-        ))}
+        {routes
+          .filter((l) => !l.index)
+          .map((l) => (
+            <li key={l.label}>
+              <Link to={l.path}>{l.label}</Link>
+            </li>
+          ))}
+        <a
+          href="https://ko-fi.com/fullstackfarzzy"
+          className="app__navbar--link"
+        >
+          <span className="app__navbar--coffee-logo">
+            <img
+              src="https://storage.ko-fi.com/cdn/cup-border.png"
+              className="kofimg"
+              alt="coffee"
+            />
+          </span>
+          Buy me a Coffee
+        </a>
       </ul>
     </nav>
     <Hamburger />
