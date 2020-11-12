b341e8a2bb74728496055f24e41a0596097f1d9e
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2020-11-12 17:01:52 -0600

    type

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,7 +4,8 @@ const data = [
     title: "Keeper Well Tool",
     subtitle: "Internal Chevron Hackathon",
     link: "/projects/dojo/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/dojo.jpg?raw=true",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/dojo.jpg?raw=true",
     date: "2019-11-20",
     desc:
       " " +
@@ -14,7 +15,8 @@ const data = [
     title: "COVID-19 Tracker",
     subtitle: "Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon",
     link: "https://covid19-tracker-6fff0.web.app/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/covidtracker.JPG?raw=true",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/covidtracker.JPG?raw=true",
     date: "2020-05-20",
     desc:
       "Dashboard's are the new cool thing! I was so inspired by these COVID trackers in order to provide valuable data insights to the world, I took a stab myself. #StayHome",
@@ -23,7 +25,8 @@ const data = [
     title: "Spotify-Clone",
     subtitle: "A clone of the famous music streaming app, Spotify",
     link: "/project/spotify/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/spotify.JPG?raw=true",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/spotify.JPG?raw=true",
     date: "2020-07-20",
     desc:
       "Every day for the past 5 years, I have used the Spotify music streaming app, whether its listening to Avicci on my morning jog, The Daily Podcast on my way to work, or Mozart while I am coding! Check out my Spotify-Clone!",
@@ -32,34 +35,39 @@ const data = [
     title: "Netflix-Clone",
     subtitle: "A netflix-clone",
     link: "https://netflix-clone-5966a.web.app/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/netflix.JPG?raw=true",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/netflix.JPG?raw=true",
     date: "2020-08-20",
     desc:
       "Netflixing - the act of watching an entire season of a show in one sitting. A total valid excuse for avoiding social obligations. Check out my Netflix-Clone!",
   },
   {
-    title: "iMessage-Clone",
-    subtitle: "An iMessage-clone",
-    link: "https://imessage-clone-f3625.web.app/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/imessage.JPG?raw=true",
-    date: "2020-10-24",
-    desc:
-      "Messages - the go-to communication for millenials to quickly transfer thoughts, emotions, and gifs to your loved ones. Check out my iMessage-Clone using Redux!",
-  },
-  {
     title: "TikTok-Clone",
     subtitle: "A TikTok-clone",
     link: "/projects/tiktok/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/tiktokpic.JPG?raw=true",
-    date: "2020-11-1",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/tiktokpic.JPG?raw=true",
+    date: "2020-09-1",
     desc:
       "Tik tok-er - This app brings out the inner Gen Z in me! Who knew that a 15 second reel of people dancing and singing would turn into a $75 Billion social media app? ",
   },
   {
+    title: "iMessage-Clone",
+    subtitle: "An iMessage-clone",
+    link: "https://imessage-clone-f3625.web.app/",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/imessage.JPG?raw=true",
+    date: "2020-10-24",
+    desc:
+      "Messages - the go-to communication for millenials to quickly transfer thoughts, emotions, and gifs to your loved ones. Check out my iMessage-Clone using Redux!",
+  },
+
+  {
     title: "Amazon-Clone",
     subtitle: "A Amazon-clone",
     link: "https://clone-1e996.web.app/",
-    image: "https://github.com/farzad528/personal-website/blob/main/public/images/projects/amazon.JPG?raw=true",
+    image:
+      "https://github.com/farzad528/personal-website/blob/main/public/images/projects/amazon.JPG?raw=true",
     date: "2020-11-12",
     desc:
       "Amazonian - Someone who spends more than 50% of there paycheck on online binge shopping. Check out my Amazon-Clone! ",
