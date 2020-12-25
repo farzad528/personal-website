46c115ed23b6df8c528c90181982a8b30bd546d4
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2020-12-25 13:08:23 -0600

    push safe popm

--- app/data/resume/certifications.js
+++ app/data/resume/certifications.js
@@ -6,14 +6,19 @@
 // These are random certifications. Please replace with your own.
 const certifications = [
   {
+    date: "December 2020",
+    content:
+      '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="a7df84ba-832e-4c1e-983e-d05f65716d09" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
+  },
+  {
     date: "August 2020",
     content:
       '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="11df610c-346b-4b80-b385-7767f30c4b30" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
   },
   {
-    date: "October 2019",
+    date: "May 2020",
     content:
-      '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="6775e73a-f71b-493d-8791-3c3ef25c92bc" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
+      '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="d423c6d8-bb47-42fc-b55c-0d9b89e0e3eb" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
   },
   {
     date: "April 2020",
@@ -21,9 +26,9 @@ const certifications = [
       '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="a408bc0b-3a81-458b-a90c-d9e0cf964743" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
   },
   {
-    date: "May 2020",
+    date: "October 2019",
     content:
-      '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="d423c6d8-bb47-42fc-b55c-0d9b89e0e3eb" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
+      '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="6775e73a-f71b-493d-8791-3c3ef25c92bc" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
   },
 ];
 
